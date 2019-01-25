99f62ef52e7f378c49a3866b2196e65fc24fde43
jhjjgu0115 <jhjjgu0115@naver.com>
2019-01-25 13:26:24 +0900

    backup point

---
+++ Source/PrototypePulse.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using RimWorld;
+using Harmony;
+using UnityEngine;
+using Verse.AI;
+using Verse;
+using Verse.Sound;
+
+namespace NewRatkin
+{
+    /*
+    [StaticConstructorOnStartup]
+    public static class LabPatch
+    {
+        private static readonly Type patchType = typeof(LabPatch);
+        static LabPatch()
+        {
+            
+            HarmonyInstance harmonyInstance = HarmonyInstance.Create("com.Lab.rimworld.mod");
+            harmonyInstance.Patch(AccessTools.Method(typeof(VerbTracker), "CreateVerbTargetCommand", null, null), null, new HarmonyMethod(patchType, "CreateVerbTargetCommandPostfix", null));
+            harmonyInstance.Patch(AccessTools.Method(typeof(Pawn_EquipmentTracker), "GetGizmos", null, null), null, new HarmonyMethod(patchType, "GetGizmosPostfix", null));
+        }
+        public static void CreateVerbTargetCommandPostfix(Command_VerbTarget __result)
+        {
+            Log.Message(__result.verb.EquipmentSource.def.ToString() + " : " + __result.verb.verbProps.burstShotCount);
+        }
+        public static void GetGizmosPostfix(Gizmo __result)
+        {
+            //Log.Message(((Command)__result).defaultLabel);
+        }
+    }*/
+
+
+
+    /*
+    public class Verb_Shoot : Verb_LaunchProjectile
+    {
+        protected override int ShotsPerBurst
+        {
+            get
+            {
+                return this.verbProps.burstShotCount;
+            }
+        }
+
+        protected override bool TryCastShot()
+        {
+            bool flag = base.TryCastShot();
+            if (flag && base.CasterIsPawn)
+            {
+                base.CasterPawn.records.Increment(RecordDefOf.ShotsFired);
+            }
+            return flag;
+        }
+    }
+
+    public class Verb_LaunchProjectile : Verb
+    {
+        public virtual ThingDef Projectile
+        {
+            get
+            {
+                if (base.EquipmentSource != null)
+                {
+                    CompChangeableProjectile comp = base.EquipmentSource.GetComp<CompChangeableProjectile>();
+                    if (comp != null && comp.Loaded)
+                    {
+                        return comp.Projectile;
+                    }
+                }
+                return this.verbProps.defaultProjectile;
+            }
+        }
+
+        public override void WarmupComplete()
+        {
+            base.WarmupComplete();
+            Find.BattleLog.Add(new BattleLogEntry_RangedFire(this.caster, (!this.currentTarget.HasThing) ? null : this.currentTarget.Thing, (base.EquipmentSource == null) ? null : base.EquipmentSource.def, this.Projectile, this.ShotsPerBurst > 1));
+            Pawn pawn = this.currentTarget.Thing as Pawn;
+            if (pawn != null && !pawn.Downed && base.CasterIsPawn && base.CasterPawn.skills != null)
+            {
+                float num = (!pawn.HostileTo(this.caster)) ? 20f : 170f;
+                float num2 = this.verbProps.AdjustedFullCycleTime(this, base.CasterPawn);
+                base.CasterPawn.skills.Learn(SkillDefOf.Shooting, num * num2, false);
+            }
+        }
+
+        protected override bool TryCastShot()
+        {
+            if (this.currentTarget.HasThing && this.currentTarget.Thing.Map != this.caster.Map)
+            {
+                return false;
+            }
+            ThingDef projectile = this.Projectile;
+            if (projectile == null)
+            {
+                return false;
+            }
+            ShootLine shootLine;
+            bool flag = base.TryFindShootLineFromTo(this.caster.Position, this.currentTarget, out shootLine);
+            if (this.verbProps.stopBurstWithoutLos && !flag)
+            {
+                return false;
+            }
+            if (base.EquipmentSource != null)
+            {
+                CompChangeableProjectile comp = base.EquipmentSource.GetComp<CompChangeableProjectile>();
+                if (comp != null)
+                {
+                    comp.Notify_ProjectileLaunched();
+                }
+            }
+            Thing launcher = this.caster;
+            Thing equipment = base.EquipmentSource;
+            CompMannable compMannable = this.caster.TryGetComp<CompMannable>();
+            if (compMannable != null && compMannable.ManningPawn != null)
+            {
+                launcher = compMannable.ManningPawn;
+                equipment = this.caster;
+            }
+            Vector3 drawPos = this.caster.DrawPos;
+            Projectile projectile2 = (Projectile)GenSpawn.Spawn(projectile, shootLine.Source, this.caster.Map, WipeMode.Vanish);
+            if (this.verbProps.forcedMissRadius > 0.5f)
+            {
+                float num = VerbUtility.CalculateAdjustedForcedMiss(this.verbProps.forcedMissRadius, this.currentTarget.Cell - this.caster.Position);
+                if (num > 0.5f)
+                {
+                    int max = GenRadial.NumCellsInRadius(num);
+                    int num2 = Rand.Range(0, max);
+                    if (num2 > 0)
+                    {
+                        IntVec3 c = this.currentTarget.Cell + GenRadial.RadialPattern[num2];
+                        this.ThrowDebugText("ToRadius");
+                        this.ThrowDebugText("Rad\nDest", c);
+                        ProjectileHitFlags projectileHitFlags = ProjectileHitFlags.NonTargetWorld;
+                        if (Rand.Chance(0.5f))
+                        {
+                            projectileHitFlags = ProjectileHitFlags.All;
+                        }
+                        if (!this.canHitNonTargetPawnsNow)
+                        {
+                            projectileHitFlags &= ~ProjectileHitFlags.NonTargetPawns;
+                        }
+                        projectile2.Launch(launcher, drawPos, c, this.currentTarget, projectileHitFlags, equipment, null);
+                        return true;
+                    }
+                }
+            }
+            ShotReport shotReport = ShotReport.HitReportFor(this.caster, this, this.currentTarget);
+            Thing randomCoverToMissInto = shotReport.GetRandomCoverToMissInto();
+            ThingDef targetCoverDef = (randomCoverToMissInto == null) ? null : randomCoverToMissInto.def;
+            if (!Rand.Chance(shotReport.AimOnTargetChance_IgnoringPosture))
+            {
+                shootLine.ChangeDestToMissWild(shotReport.AimOnTargetChance_StandardTarget);
+                this.ThrowDebugText("ToWild" + ((!this.canHitNonTargetPawnsNow) ? string.Empty : "\nchntp"));
+                this.ThrowDebugText("Wild\nDest", shootLine.Dest);
+                ProjectileHitFlags projectileHitFlags2 = ProjectileHitFlags.NonTargetWorld;
+                if (Rand.Chance(0.5f) && this.canHitNonTargetPawnsNow)
+                {
+                    projectileHitFlags2 |= ProjectileHitFlags.NonTargetPawns;
+                }
+                projectile2.Launch(launcher, drawPos, shootLine.Dest, this.currentTarget, projectileHitFlags2, equipment, targetCoverDef);
+                return true;
+            }
+            if (this.currentTarget.Thing != null && this.currentTarget.Thing.def.category == ThingCategory.Pawn && !Rand.Chance(shotReport.PassCoverChance))
+            {
+                this.ThrowDebugText("ToCover" + ((!this.canHitNonTargetPawnsNow) ? string.Empty : "\nchntp"));
+                this.ThrowDebugText("Cover\nDest", randomCoverToMissInto.Position);
+                ProjectileHitFlags projectileHitFlags3 = ProjectileHitFlags.NonTargetWorld;
+                if (this.canHitNonTargetPawnsNow)
+                {
+                    projectileHitFlags3 |= ProjectileHitFlags.NonTargetPawns;
+                }
+                projectile2.Launch(launcher, drawPos, randomCoverToMissInto, this.currentTarget, projectileHitFlags3, equipment, targetCoverDef);
+                return true;
+            }
+            ProjectileHitFlags projectileHitFlags4 = ProjectileHitFlags.IntendedTarget;
+            if (this.canHitNonTargetPawnsNow)
+            {
+                projectileHitFlags4 |= ProjectileHitFlags.NonTargetPawns;
+            }
+            if (!this.currentTarget.HasThing || this.currentTarget.Thing.def.Fillage == FillCategory.Full)
+            {
+                projectileHitFlags4 |= ProjectileHitFlags.NonTargetWorld;
+            }
+            this.ThrowDebugText("ToHit" + ((!this.canHitNonTargetPawnsNow) ? string.Empty : "\nchntp"));
+            if (this.currentTarget.Thing != null)
+            {
+                projectile2.Launch(launcher, drawPos, this.currentTarget, this.currentTarget, projectileHitFlags4, equipment, targetCoverDef);
+                this.ThrowDebugText("Hit\nDest", this.currentTarget.Cell);
+            }
+            else
+            {
+                projectile2.Launch(launcher, drawPos, shootLine.Dest, this.currentTarget, projectileHitFlags4, equipment, targetCoverDef);
+                this.ThrowDebugText("Hit\nDest", shootLine.Dest);
+            }
+            return true;
+        }
+
+        private void ThrowDebugText(string text)
+        {
+            if (DebugViewSettings.drawShooting)
+            {
+                MoteMaker.ThrowText(this.caster.DrawPos, this.caster.Map, text, -1f);
+            }
+        }
+
+        private void ThrowDebugText(string text, IntVec3 c)
+        {
+            if (DebugViewSettings.drawShooting)
+            {
+                MoteMaker.ThrowText(c.ToVector3Shifted(), this.caster.Map, text, -1f);
+            }
+        }
+
+        public override float HighlightFieldRadiusAroundTarget(out bool needLOSToCenter)
+        {
+            needLOSToCenter = true;
+            ThingDef projectile = this.Projectile;
+            if (projectile == null)
+            {
+                return 0f;
+            }
+            return projectile.projectile.explosionRadius;
+        }
+
+        public override bool Available()
+        {
+            if (!base.Available())
+            {
+                return false;
+            }
+            if (base.CasterIsPawn)
+            {
+                Pawn casterPawn = base.CasterPawn;
+                if (casterPawn.Faction != Faction.OfPlayer && casterPawn.mindState.MeleeThreatStillThreat && casterPawn.mindState.meleeThreat.Position.AdjacentTo8WayOrInside(casterPawn.Position))
+                {
+                    return false;
+                }
+            }
+            return this.Projectile != null;
+        }
+    }*/
+}
