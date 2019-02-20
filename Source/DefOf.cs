19a1cc270b6990ceac6a88b6f8cff6964406b78d
jhjjgu0115 <jhjjgu0115@naver.com>
2019-02-20 11:30:07 +0900

    dummy data remove

---
+++ Source/DefOf.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+using Verse.AI.Group;
+using Verse.Sound;
+using System.Text;
+using System.Reflection;
+using RimWorld;
+using Harmony;
+using UnityEngine;
+using Verse.AI;
+
+namespace NewRatkin
+{
+    [DefOf]
+    public static class RatkinBuildingDefOf
+    {
+        //public static PawnKindDef RatkinDemonMan;
+        //public static PawnKindDef RatkinEliteSoldier;
+        //public static ThingDef RK_GuerrillaTunnel;
+    }
+
+    [DefOf]
+    public static class WeaponDefOf
+    {
+        //public static ThingDef RK_FaceCleaner;
+    }
+
+
+
+}
