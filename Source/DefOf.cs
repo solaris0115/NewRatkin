cfdcc7617f698d5f3c1f9912e3132f727c4f704a
jhjjgu0115 <jhjjgu0115@naver.com>
2019-02-22 13:21:32 +0900

    Revert "Merge remote-tracking branch 'refs/remotes/origin/Saint_Tail'"
    This reverts commit ae86b4d840dbc3113619c4e27ee0d30abaf0cff0, reversing
    changes made to 19a1cc270b6990ceac6a88b6f8cff6964406b78d.

--- Source/DefOf.cs
+++ Source/DefOf.cs
@@ -14,44 +14,17 @@ using Verse.AI;
 namespace NewRatkin
 {
     [DefOf]
-    public static class RatkinPawnKindDefOf
-    {
-        public static PawnKindDef RatkinNoble;
-
-        public static PawnKindDef RatkinColonist;
-
-        public static PawnKindDef RatkinServant;
-
-        public static PawnKindDef RatkinCombatant;
-
-        public static PawnKindDef RatkinSoldier;
-
-        public static PawnKindDef RatkinSubject;
-
-        public static PawnKindDef RatkinMercenary;
-
-        public static PawnKindDef RatkinEliteGuardener;
-
-        public static PawnKindDef RatkinPriest;
-
-        public static PawnKindDef RatkinMerchant;
-
-        public static PawnKindDef RatkinMurderer;
-
-        //괴도 찍찍이 확장팩
-        public static PawnKindDef RatkinDemonMan;
-        public static PawnKindDef RatkinEliteSoldier;
-    }
-    [DefOf]
     public static class RatkinBuildingDefOf
     {
-        public static ThingDef RK_GuerrillaTunnel;
+        //public static PawnKindDef RatkinDemonMan;
+        //public static PawnKindDef RatkinEliteSoldier;
+        //public static ThingDef RK_GuerrillaTunnel;
     }
 
     [DefOf]
-    public static class RatkinWeaponDefOf
+    public static class WeaponDefOf
     {
-        public static ThingDef RK_FaceCleaner;
+        //public static ThingDef RK_FaceCleaner;
     }
 
 
