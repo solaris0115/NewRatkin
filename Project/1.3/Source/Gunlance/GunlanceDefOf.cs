f55f04e1777e1f8643017688de10368b655d12ec
solaris0115 <solaris0115@naver.com>
2021-07-23 02:53:18 +0900

    build 1.3

---
+++ Project/1.3/Source/Gunlance/GunlanceDefOf.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using RimWorld;
+using HarmonyLib;
+using UnityEngine;
+using Verse.AI;
+using Verse.Sound;
+using Verse;
+
+
+namespace NewRatkin
+{
+    [DefOf]
+    public static class GunlanceDefOf
+    {
+        public static ThingDef GunlanceExplosion;
+        public static ThingDef GunlancePreIgnition;
+        public static ThingDef GunlanceAfterIgnition;
+    }
+    [DefOf]
+    public static class RatkinSoundDefOf
+    {
+        public static SoundDef RK_Charge;
+        public static SoundDef RK_Fire;
+        public static SoundDef RK_OverHeat;
+        public static SoundDef RK_Reload;
+        public static SoundDef RK_WyvernFire;
+    }
+}
---
+++ Project/1.3/Source/Priest/LordToil_JoinPrayerService.cs
@@ -0,0 +1,31 @@
+﻿using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+
+namespace NewRatkin
+{
+    public class LordToil_JoinPrayerService : LordToil
+    {
+        Pawn organizer;
+        IntVec3 spot;
+
+        public LordToil_JoinPrayerService(Pawn organizer, IntVec3 spot)
+        {
+            this.organizer = organizer;
+            this.spot = spot;
+        }
+
+        public override ThinkTreeDutyHook VoluntaryJoinDutyHookFor(Pawn p)
+        {
+            return RatkinDutyDefOf.RK_JoinPrayerService.hook;
+        }
+
+		public override void UpdateAllDuties()
+		{
+			foreach (var pawn in lord.ownedPawns)
+			{
+				pawn.mindState.duty = new PawnDuty(RatkinDutyDefOf.RK_JoinPrayerService, spot);
+			}
+		}
+	}
+}
