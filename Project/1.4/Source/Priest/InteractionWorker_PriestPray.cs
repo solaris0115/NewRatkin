1a2558a5c35a0d1f15e85228a6df268d0f9f367d
solaris <solaris0115@gmail.com>
2022-10-24 20:45:06 +0900

    create 1.4 version

---
+++ Project/1.4/Source/Priest/InteractionWorker_PriestPray.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace NewRatkin
+{
+    public class InteractionWorker_PriestPray : InteractionWorker
+	{
+		public override float RandomSelectionWeight(Pawn initiator, Pawn recipient)
+		{
+			if (initiator.story.adulthood != null && initiator.story.adulthood == BackstoryCache.Ratkin_Sister)
+			{
+				return ConstPriest.InteractionPrayWeight;
+			}
+
+			return 0f;
+		}
+	}
+}
--- SymbolicLink.bat
+++ SymbolicLink.bat
@@ -1,3 +1,3 @@
-mklink /d "E:\Program Files (x86)\SteamLibrary\steamapps\common\RimWorld\Mods\NewRatkin" "E:\GitProject\NewRatkin\Project"
-::mklink /d "Input_your_steamLibrary_Path\steamapps\common\RimWorld\Mods\NewRatkin" "Download_Path\NewRatkin\Project"
+rmdir "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\NewRatkin"
+mklink /d  "C:\Program Files (x86)\Steam\steamapps\common\RimWorld\Mods\NewRatkin" D:\GitProject\NewRatkin\Project
 pause
