ee541eeee6fd7bc7569b5c5d47ddfe8cd5e16cd2
solaris <solaris0115@gmail.com>
2023-02-21 00:19:57 +0900

    Revert "Merge pull request #79 from kingerman88/release"
    This reverts commit 63359abe62c0de9917dfbc39a1ae827e1c74fe65, reversing
    changes made to e13c5342bee8f89dae55e14d83413c00978d840e.

--- Project/1.4/Source/ShieldOfRatkinia/StockGen_Mercenary.cs
+++ Project/1.4/Source/ShieldOfRatkinia/StockGen_Mercenary.cs
@@ -28,13 +28,11 @@ namespace NewRatkin
                 }
                 var mercenaryPawnKindDef = RatkinPawnKindDefOf.RatkinMercenary;
 
-                PawnGenerationRequest request = new PawnGenerationRequest(
-                    kind: mercenaryPawnKindDef,
-                    context: PawnGenerationContext.NonPlayer,
-                    faction: mercenaryFaction,
-                    tile: forTile,
-                    forceAddFreeWarmLayerIfNeeded: !this.trader.orbital
-                );
+                PawnGenerationRequest request = new PawnGenerationRequest();
+                request.KindDef = mercenaryPawnKindDef;
+                request.Faction = mercenaryFaction;
+                request.Tile = forTile;
+                request.ForceAddFreeWarmLayerIfNeeded = !this.trader.orbital;
 
                 yield return PawnGenerator.GeneratePawn(request);
             }
